baf2f8e0bd3d2f5ca49e7e842da85089e0d3a0d6
chijing <chijing.leow@selu.edu>
2021-11-11 20:44:11 +0000

    project 2

--- R/Clean_up.R
+++ R/Clean_up.R
@@ -1,2 +1,2 @@
-Inputs: dataset
-expected outputs: dataset without NA values and maybe format the data to a more managable way..
+#Inputs: rdata
+#expected outputs: formatted data with created DGElist for count data. 
