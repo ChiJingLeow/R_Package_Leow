0f2157857950e7beecf58b354a7ef8c56352b582
chijing <chijing.leow@selu.edu>
2021-11-11 19:57:58 +0000

    new change

--- R/Clean_up.R
+++ R/Clean_up.R
@@ -1,2 +1,2 @@
 Inputs: dataset
-expected outputs: dataset without NA values and maybe format the data to a more managable way.
+expected outputs: dataset without NA values and maybe format the data to a more managable way..
