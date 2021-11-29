f1eb7b11f2a15fad03d1d72d2e0ca5c35fc994b9
chijing <chijing.leow@selu.edu>
2021-11-29 17:12:41 +0000

    first function

--- R/Clean_up.R
+++ R/Clean_up.R
@@ -1,2 +1,11 @@
-#Inputs: rdata
-#expected outputs: formatted data with created DGElist for count data. 
+#'remove potential NA values in rdata
+#'
+#'@params data - your working rdata
+#'@params product - the data without NA values 
+#
+
+clean_up <- function(data) {
+  product <- na.omit(data)
+  NA_omit_rdata <- data
+  return(NA_omit_rdata)
+}
